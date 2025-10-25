940eea4081c3a2494fffa2a1837826b9a7d0f970
Estefany Ponce <164693621+EstefanyPonce24002@users.noreply.github.com>
2025-10-24 21:50:49 -0600

    Delete api-plataforma-cursos-online directory

--- api-plataforma-cursos-online/app/src/main/java/org/ues/api/plataformacursos/exception/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package org.ues.api.plataformacursos.exception;
-
-// Indica que un recurso no fue encontrado
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
--- api-plataforma-cursos-online/app/src/main/java/org/ues/api/plataformacursos/repository/InscripcionRepository.java
+++
@@ -1,8 +0,0 @@
-package org.ues.api.plataformacursos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.ues.api.plataformacursos.model.Inscripcion;
-
-// Permite manejar las inscripciones directamente con los métodos estándar de Spring Data JPA.
-public interface InscripcionRepository extends JpaRepository<Inscripcion, Long> {
-}
